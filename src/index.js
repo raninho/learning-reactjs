cc0eb2912c5c2f40909d5641e7899184db1fb107
Raniere Fernandes de Medeiros <raninho@raninho.com.br>
2016-12-23 10:26:06 -0300

    src/index: Refactoring App to Class.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyDQtEiHD1kYHHmEssrs55UzViL79Wr9g48';
 
-const App = () => {
-  return (
-    <div>
-    <SearchBar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
