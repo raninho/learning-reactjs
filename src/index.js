3ab2fcf43ee6e454007ed8d4e68b980b780ae7cd
Raniere Fernandes de Medeiros <raninho@raninho.com.br>
2016-12-23 10:29:55 -0300

    src/index: Added YTSearch and State.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
-import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDQtEiHD1kYHHmEssrs55UzViL79Wr9g48';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {videos: []}
+    YTSearch({key: API_KEY, term: 'brazil'}, (videos) => {
+      this.setState({videos});
+    });
+  }
   render() {
     return (
       <div>
